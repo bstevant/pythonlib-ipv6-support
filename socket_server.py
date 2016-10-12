de182bbe7d77d89ac9a31e6a1341465e5b367563
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2016-10-12 10:55:39 +0200

    Add patch Enhance socket server

--- socket_server.py
+++ socket_server.py
@@ -5,32 +5,40 @@ import socket
 import sys
 
 #HOST = None               # Symbolic name meaning all available interfaces, open tcp46 on IPv6-enabled host
-HOST = 'localhost'        # Only open tcp6 on MacOSX ?
+HOST = 'localhost'
 PORT = 50007              # Arbitrary non-privileged port
 s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
+
+try:
+    res = socket.getaddrinfo(HOST, PORT, socket.AF_INET6,
+                             socket.SOCK_STREAM, 0, socket.AI_PASSIVE|socket.AI_V4MAPPED|socket.AI_ADDRCONFIG)
+except socket.gaierror as msg:
+    print('Could not resolve hostname "{}": {}'.format(HOST, msg))
+    sys.exit(1)
+for af, socktype, proto, canonname, sa in res:
     try:
         s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        s = None
-        continue
-    try:
         s.bind(sa)
-        s.listen(1)
+        break
     except socket.error as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print('could not open socket')
+        if not s:
+            print('Could not create socket: {}'.format(msg))
+        else:
+            print('Could not bind socket: {}'.format(msg))
+            s.close()
+            s = None
+else:
+    print('No socket could be created/bound')
     sys.exit(1)
+del res
+
+print('Bound to {}'.format(s.getsockname()))
+s.listen(1)
 conn, addr = s.accept()
-print('Connected by', addr)
+print('Connection from {}'.format(addr))
 while True:
     data = conn.recv(1024)
     if not data: break
     conn.send(data)
 conn.close()
+s.close()
